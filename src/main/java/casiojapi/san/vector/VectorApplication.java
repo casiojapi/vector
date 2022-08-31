720b16ad9c51419ddbee90f36d0a7fc715cbf6e1
casiojapi <maximosantoro@protonmail.com>
2022-08-31 20:26:19 -0300

    init 2

---
+++ src/main/java/casiojapi/san/vector/VectorApplication.java
@@ -0,0 +1,13 @@
+package casiojapi.san.vector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/casiojapi/san/vector/VectorApplicationTests.java
@@ -0,0 +1,13 @@
+package casiojapi.san.vector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
